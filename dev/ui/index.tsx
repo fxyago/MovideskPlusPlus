f47579e5783acd58587a0504cf583651df73dcc4
Yago MS Bardini <yago.bardini@cloudworks.com.br>
2025-06-22 22:27:04 -0300

    - Movido pasta dev/ui para fora de src - Reformatado configs nodemon

--- dev/ui/index.tsx
+++ dev/ui/index.tsx
@@ -1,5 +1,5 @@
-import { MainMenu } from '@/pages/content/Menu';
 import { createRoot } from 'react-dom/client';
+import { MainMenu } from '../../src/pages/content/Menu';
 
 const root = document.getElementById('root');
 if (root) createRoot(root).render(<MainMenu />);
--- vite.config.ui.ts
+++ vite.config.ui.ts
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  root: './src/dev/ui/',
+  root: './dev/ui/',
   plugins: [tailwindcss(), tsconfigPaths(), react()],
   publicDir: resolve(__dirname, 'public'),
 });
